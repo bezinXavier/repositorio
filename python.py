c7fd399142f58d307815fcf4e5d9ac1f18f561c0
Bernardo Xavier <141173332+bezinXavier@users.noreply.github.com>
2023-09-05 14:58:55 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+numero = 10
+
+nome = "joão"
+
+booleano = True
+
+a = 10
+
+b = 5
+
+soma = a + b
+
+subtracao = a - b
+
+multiplicacao = a * b
+
+divisao = a / b
+
+va = True
+vb = False
+
+r1 = va and vb 
+r2 = va or vb
+r3 = not va
+
+c = 3
+
+r4 = c > 4
+r5 = c == 4
+r6 = c != 5
+
+r7 = c > 1 and nome == "joão"
+
+
+
+
+
