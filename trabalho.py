8e3bafecba5aec92bd416e146d43e61c24193638
Bernardo Xavier <141173332+bezinXavier@users.noreply.github.com>
2023-10-24 14:48:56 -0300

    Add files via upload

---
+++ trabalho.py
@@ -0,0 +1,18 @@
+def simples(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+
+def medio(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+else:
+        return 'tente outra cor'
+
+def completo(cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    elif cor == 'marrom':
+        return 'não tem salvação'
+    else:
+        return 'tente outra cor'
+    
